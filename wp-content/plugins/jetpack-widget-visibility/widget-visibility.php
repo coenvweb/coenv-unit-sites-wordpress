b4e8eb0912db780a1d84609adf9471bc9d1ee31e
joeeastham <jeastham@gmail.com>
2014-11-22 14:50:30 -0800

    Adding plugins

--- wp-content/plugins/jetpack-widget-visibility/widget-visibility.php
+++ wp-content/plugins/jetpack-widget-visibility/widget-visibility.php
@@ -4,9 +4,8 @@
  * Plugin Name: JP Widget Visibility
  * Plugin URI: http://wordpress.org/plugins/jetpack-widget-visibility/
  * Description: Control what pages your widgets appear on.
- * Author: Anas H. Sulaiman
- * Version: 3.1
- * Author URI: http://ahs.pw/
+ * Author: JP Bot
+ * Version: 3.2
  * Text Domain: jetpack
  * Domain Path: /languages/
  * License: GPL2 or later
@@ -25,14 +24,11 @@
 
 include dirname( __FILE__ ) . "/widget-visibility/widget-conditions.php";
 
-// E-1 {
 function jetpack_widget_visibility_load_textdomain() {
 	load_plugin_textdomain( 'jetpack', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
 add_action( 'plugins_loaded', 'jetpack_widget_visibility_load_textdomain' );
-// } E-1
 
-// E-2 {
 function jetpack_widget_visibility_settings_link($actions) {
 	return array_merge(
 		array( 'settings' => sprintf( '<a href="%s">%s</a>', 'widgets.php', __( 'Widgets', 'jetpack' ) ) ),
@@ -41,10 +37,3 @@ function jetpack_widget_visibility_settings_link($actions) {
 	return $actions;
 }
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'jetpack_widget_visibility_settings_link' );
-// } E-2
-
-/*
-Edits by Anas H. Sulaiman:
-E-1 : load text domain
-E-2 : add settings link
-*/
