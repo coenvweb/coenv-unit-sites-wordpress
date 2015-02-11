2e4a2f219f970f34ee0f8677ba8707bf3be20ac5
drewcoll <drewcoll@gmail.com>
2015-02-11 11:08:33 -0800

    Updates for jack's list

--- footer.php
+++ footer.php
@@ -19,15 +19,15 @@
 					<?php get_search_form() ?>
 					<div class="social-buttons">
 					<?php if (get_option('facebook')) { ?>
-						<a class="facebook button" href="<?php echo get_option('facebook'); ?>" title="Join us on Facebook">
+						<a class="facebook" href="<?php echo get_option('facebook'); ?>" title="Join us on Facebook">
 							<i class="fi-social-facebook"></i>
 						</a><?php } ?>
 					<?php if (get_option('twitter')) { ?>
-						<a class="twitter button" href="<?php echo 'http://twitter.com/' . get_option('twitter'); ?>" data-site-twitter="<?php echo get_option('twitter'); ?>" title="Join us on Twitter">
+						<a class="twitter" href="<?php echo 'http://twitter.com/' . get_option('twitter'); ?>" data-site-twitter="<?php echo get_option('twitter'); ?>" title="Join us on Twitter">
 								<i class="fi-social-twitter"></i>
 						</a><?php } ?>
 					<?php if (get_option('youtube')) { ?>
-						<a class="youtube button" href="<?php echo get_option('youtube'); ?>" title="Join us on YouTube">
+						<a class="youtube" href="<?php echo get_option('youtube'); ?>" title="Join us on YouTube">
 								<i class="fi-social-youtube"></i>
 						</a><?php } ?>
 					</div>
