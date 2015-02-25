8f60c6829ad0b24d3d205a72547562d8b92093a0
drewcoll <drewcoll@gmail.com>
2015-02-25 15:16:55 -0800

    print style updates

--- templates/faculty.php
+++ templates/faculty.php
@@ -26,7 +26,7 @@ if ($cats) {
      echo '<h3 class="cat-title">What we work on:</h3>';
      echo '<ul class="medium-block-grid-2 fac-cats">';
      foreach($cats as $cat) { 
-          echo '<li><a class="button" href="/faculty-research/research-areas/' . $cat->slug . '/">' . $cat->name . '</a></li>';
+          echo '<li><a class="button" href="/faculty-and-research/research-areas/' . $cat->slug . '/">' . $cat->name . '</a></li>';
      }
      echo '</ul>';
      echo '</div>';
