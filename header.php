2028182498d730861ca7eb3cc72c3f557ce2440d
joeeastham <jeastham@gmail.com>
2015-03-25 14:33:23 -0700

    Adding ga script. Currently commented out.

--- header.php
+++ header.php
@@ -79,6 +79,18 @@
 	<meta property="og:url" content="<?php echo $post_link ?>" />
 	<meta property="og:image" content="<?php echo $post_image ?>" />
 	<meta property="og:site_name" content="<?php bloginfo('name') ?>" />
+
+  <script>
+  /*
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-21836351-1', 'auto');
+    ga('send', 'pageview');
+  */
+  </script>
   </head>
   <body <?php body_class($banner_class); ?>>
   
