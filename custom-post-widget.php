2eb005173f97d7b1327b713f6048c8d76a45f9ad
coenvweb <coenvweb@uw.edu>
2015-06-17 10:31:33 -0700

    Add local file link type and move links into content div

--- custom-post-widget.php
+++ custom-post-widget.php
@@ -16,14 +16,14 @@ $widget_img_attr = array(
 	'alt'	=> trim( strip_tags( $attachment->post_excerpt ) ),
 	'title'	=> trim( strip_tags( $attachment->post_title ) ),
 );
-$widget_img = get_the_post_thumbnail( $content_post -> ID, 'sm-sq');
-$widget_copy = get_field('block_text', $content_post -> ID);
-$rows = get_field('add_links', $content_post -> ID);
+$widget_img = get_the_post_thumbnail( $content_post -> ID, 'medium');
+$widget_copy = get_field( 'block_text', $content_post -> ID );
+$rows = get_field( 'add_links', $content_post -> ID );
 
 if($rows) {
     $buttons = '<ul class="widget_links">';
-    foreach($rows as $row) {
-        if($row['link_type'] == 'internal') {
+    foreach( $rows as $row ) {
+        if( $row['link_type'] == 'internal' ) {
             $link_title =  $row['link_to_a_page_on_this_site'][0]['link_title_internal'];
             $link_url = get_permalink($row['link_to_a_page_on_this_site'][0]['select_page'][0]);
             $link_target = 'self';	
@@ -33,7 +33,11 @@ if($rows) {
             $link_url = $row['link_to_an_external_site'][0]['link_url'];
             $link_target ='blank';
             $buttons .= '<li><a class="button"  title="' . $link_title . '" href="' . $link_url . '" target="_' . $link_target . '">' . $link_title . '</a></li>';
-        } 
+        } elseif ($row['link_type'] == 'file') {
+            $link_title = $row['link_to_or_upload_file'][0]['link_title'];
+            $link_url = $row['link_to_or_upload_file'][0]['choose_file'];
+            $buttons .= '<li><a class="button"  title="' . $link_title . '" href="' . $link_url . '" target="_' . $link_target . '">' . $link_title . '</a></li>';
+        }
     }
     $buttons .= '</ul>';
 }
@@ -45,7 +49,7 @@ echo $before_widget;
 
 if ( $show_featured_image ) {
 	echo '<div class="widget_img">';
-	echo '<a title="' . $first_link_title . '" href="' . $first_link_url . '" target="_' . $first_link_target . '">' . $widget_img . '</a>';
+	echo $widget_img;
 	echo '</div>';
 	}
 echo '<div class="widget_content">';
@@ -58,8 +62,9 @@ if ( $show_custom_post_title ) {
 	echo $after_title;
 }
 echo $widget_copy;
-echo '</div>';
 if ( $link_position == null ) {
-	echo $buttons;
+    echo $buttons;
 }
+echo '</div>';
+
 echo $after_widget;
