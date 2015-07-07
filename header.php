573aa535077aa1e1311320675b16198fc10015c9
drewcoll <drewcoll@gmail.com>
2015-07-07 09:27:37 -0700

    IE selector-limited stylesheet added

--- header.php
+++ header.php
@@ -23,7 +23,11 @@
     } ?></title>
     
   <script src="//www.washington.edu/static/alert.js" type="text/javascript"></script>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ; ?>/css/app.css" />
+
+            <!--[if IE 9]><link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ; ?>/css/appie-blessed1dev.css" /><![endif]-->
+          <!--[if IE 9]><link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ; ?>/css/appie.css" /><![endif]-->
+      
+    <!--[if !IE]><!--><link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ; ?>/css/app.css" /><!--<![endif]-->
       
     <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri() ?>/assets/img/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri() ?>/assets/img/apple-icon-60x60.png">
