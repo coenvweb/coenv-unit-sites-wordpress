b65cfea0a458d976acf7d634a4ccf9df4b292aea
drewcoll <drewcoll@gmail.com>
2015-08-19 16:50:28 -0700

    some faculty style updates

--- header.php
+++ header.php
@@ -235,7 +235,7 @@
 
 <?php if (!is_front_page() && !is_search() && !is_404()) : ?>
 <div class="container" role="document">
-    <?php if (($banner) && ( $post->post_parent==0 ) && (!is_single()) && (!is_page_template( 'templates/news.php' ) )) {
+    <?php if (($banner) && ( $post->post_parent==0 ) && (!is_single()) && (!is_page_template( 'templates/news.php' ) && (!is_page_template( 'templates/faculty.php' ))  )) {
             echo '<div class="page-row"';
       } else {
             echo '<div class="page-row mini"';
@@ -247,8 +247,12 @@
     <div class="header-title-container show-for-medium-up">
         <div class="page-title-row row">
             <div class="page-title"><h1><span>
-                <?php if ((!is_single()) && (!is_page_template( 'templates/news.php' ) )) {
+                <?php if ((!is_single()) && (!is_page_template( 'templates/news.php' ) ) ) {
+         if (!is_singular('faculty')) {
             echo the_title();
+         } else {
+             echo '<a href="/faculty-research/faculty-instructor-bios/" title="Faculty & Research">Faculty & Research</a>';
+         }
       } else {
             echo '<a href="/about/news-events/" title="News & Events">News & Events</a>';
       };?></span></h1></div>
