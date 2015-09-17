52c3db70278e35c8755980fc375a39060670d838
coenvweb <coenvweb@uw.edu>
2015-09-17 10:23:15 -0700

    month offset for date filters

--- templates/news.php
+++ templates/news.php
@@ -10,9 +10,11 @@ $url_current = $url = preg_replace('/\?.*/', '', $_SERVER['REQUEST_URI']);
  */
 
 // Dates
-$coenv_year = urlencode(htmlentities($_GET['coenv-year']));
-$coenv_month = urlencode(htmlentities($_GET['coenv-month']));
-$coenv_date = date('F Y',mktime(0,0,0,(int)$coenv_month,0,(int)$coenv_year));
+$coenv_year = (int) urlencode(htmlentities($_GET['coenv-year']));
+$coenv_month = (int) urlencode(htmlentities($_GET['coenv-month']));
+
+// Month needs an offset because php and WordPress calculate dates differently.
+$coenv_date = date('F Y',mktime(10,0,0,$coenv_month+1,0,$coenv_year));
 
 //Categories
 $coenv_cat_1 = urlencode(htmlentities($_GET['tax']));
