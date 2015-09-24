56c40f1fbc6c0489bddec12d2a49510470490e49
drewcoll <drewcoll@gmail.com>
2015-09-24 16:42:09 -0700

    google analytics added

--- header.php
+++ header.php
@@ -48,6 +48,16 @@
     <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri() ?>/assets/img/ms-icon-144x144.png">
     <meta name="theme-color" content="#4b2e84">
       
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-67870886-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+      
     <!--<script type="text/javascript" src="//use.typekit.net/dyq8fxo.js"></script>
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>-->
 
