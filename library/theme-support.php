ff55812dfeec64b9d4b68f268af0b1d2a7bc3f01
Joe Eastham <coenvweb@uw.edu>
2015-11-02 16:10:13 -0800

    Section title fixes

--- library/theme-support.php
+++ library/theme-support.php
@@ -215,10 +215,17 @@ function coenv_banner() {
 function coenv_base_section_title($id) {
 
     $coenv_post = get_post($id);
-    //print_r($coenv_post);
     $coenv_post_section = get_post(array_pop(get_post_ancestors($id)));
 
-    if (coenv_base_post_parent($id)):
+    if (is_404()):
+        $section_title = '<div class="section-title"><h2>Not Found (404)</h2></div>';
+    elseif (is_search()):
+        $section_title = '<div class="section-title"><h2>Search</h2></div>';
+     elseif ( $coenv_post->post_type == 'publications' ):
+        $section_title = '<div class="section-title"><h2><a href="/resources/">Resources</a></h2></div>';
+     elseif ( $coenv_post->post_type == 'datasets' ):
+        $section_title = '<div class="section-title"><h2><a href="/resources/">Datasets</a></h2></div>';
+    elseif (coenv_base_post_parent($id)):
         $section_title = '<div class="section-title"><h2><a href="/' . $coenv_post_section->post_name . '">' . $coenv_post_section->post_title . '</a></h2></div>';
     elseif (!is_front_page()):
         $section_title = '<div class="section-title"><h2><a href="/' . $coenv_post_section->post_name . '">' . $coenv_post_section->post_title . '</a></h2></div>';
--- search.php
+++ search.php
@@ -1,15 +1,31 @@
 <?php get_header(); ?>
 <div class="row">
-	<div class="small-12 medium-8 columns" role="main">
+	<div class="small-12 medium-12 columns" role="main">
 	
 		<?php do_action('foundationPress_before_content'); ?>
 	
-		<h2><?php _e('Search Results for', 'FoundationPress'); ?> "<?php echo get_search_query(); ?>"</h2>
+		<?php if ( have_posts() ) : ?>
+        <div class="search-filter">
 	
-	<?php if ( have_posts() ) : ?>
+		<?php do_action('foundationPress_before_content'); ?>
+	
+		<h3><?php _e('Results for', 'FoundationPress'); ?> "<?php echo get_search_query(); ?>"</h3>
+        
+        <?php get_search_form(); ?>
+        </div>
 	
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
+		<h2><a href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a></h2>
+		<p>
+		<?php
+		$teaser_limited = get_the_excerpt();
+		$teaser_limited = strip_tags($teaser_limited);
+		$teaser_limited = trim($teaser_limited, '!,?.&nbsp;') . '...';
+		if ($teaser_limited != "..."):
+		echo $teaser_limited;
+		endif;
+		?>
+		</p>
 		<?php endwhile; ?>
 		
 		<?php else : ?>
@@ -30,6 +46,5 @@
 	<?php do_action('foundationPress_after_content'); ?>
 
 	</div>
-	<?php get_sidebar(); ?>
 		
 <?php get_footer(); ?>
