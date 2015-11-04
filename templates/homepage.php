a6c0f28d7f81e77baa24afd0a2d819987f2be7fb
drewcoll <drewcoll@gmail.com>
2015-11-04 13:22:44 -0800

    more trouble

--- templates/homepage.php
+++ templates/homepage.php
@@ -241,7 +241,7 @@ $wp_query = new WP_Query( $home_args );
     </div>
 <?php endif; ?>
 <div class="large-4 columns">
-    <?php the_widget('wptt_TwitterTweets', 'title=twitter&name=CIG_UW&consumerKey=ivTXKU52g6Sx851RzmILf1svw&consumerSecret=6zVP3WDKPFVLhi8cI7XwiR0GNyGQF6ZIZa7mKULsxTiF6UrqdM&accessToken=47968457-Ccfpdt7oaFyoPQyOU5aNa8utxUXIzOIyKUWbAkXup&accessTokenSecret=BbefqWcc8WLG4s1iFQ1JkRWohhs0XJOXekbsGxnHjgtkV'); ?>
+    <?php the_widget('wptt_TwitterTweets', 'title=twitter&name=UWPCC&consumerKey=ivTXKU52g6Sx851RzmILf1svw&consumerSecret=6zVP3WDKPFVLhi8cI7XwiR0GNyGQF6ZIZa7mKULsxTiF6UrqdM&accessToken=47968457-Ccfpdt7oaFyoPQyOU5aNa8utxUXIzOIyKUWbAkXup&accessTokenSecret=BbefqWcc8WLG4s1iFQ1JkRWohhs0XJOXekbsGxnHjgtkV'); ?>
 </div>
 </div>  
         <!--&tweets_cnt=3&tweet_theme=light&tweet_border=false&store_time=4&replies_excl=true&disp_scr_name=false&dataLang=en&timeRef=false&timeAgo=true&twitterIntents=false&twitterIntentsText=false&intentColor=#999&showAvatar=false&border_rad=false&slide_style=list-->
