622d7e121882b022956f2e548003d8ccaa26169a
Cole Bessee <cbessee@uw.edu>
2016-05-09 09:37:15 -0700

    Add second link and fix form validation error feedback for content widgets

--- wp-content/plugins/coenv-widgets/coenv-widgets.php
+++ wp-content/plugins/coenv-widgets/coenv-widgets.php
@@ -34,6 +34,10 @@ class coenv_base_content extends WP_Widget {
         add_action('admin_enqueue_scripts', array($this, 'coenv_widget_scripts'));
     }
 
+    public function validateLink($link) {
+        return filter_var($link, FILTER_VALIDATE_URL);
+    }
+
     /*
     *   js and css assets
     */
@@ -42,8 +46,6 @@ class coenv_base_content extends WP_Widget {
         wp_enqueue_script('media-upload');
         wp_enqueue_script('coenv_static_widget', plugin_dir_url(__FILE__) . 'upload-media.js', array('jquery'));
     }
-
-
 
     /*
         Determine if a link is internal or external and return the correct target
@@ -99,7 +101,7 @@ class coenv_base_content extends WP_Widget {
 
                         echo $args['after_title'];
                     }
-        
+
                     if(!empty($instance['link'])) {
                         echo '</a>';
                     }
@@ -108,12 +110,16 @@ class coenv_base_content extends WP_Widget {
                         echo apply_filters('the_content', $instance['content']);
                     }
 
-                    if(!empty($instance['link']) && !empty($instance['linktext'])) {
+                   echo "<ul class='widget_links'>";
+                   if(!empty($instance['link']) && !empty($instance['linktext'])) {
                         $target = $this->getLinkTarget($instance['link'], home_url());
-                        echo "<ul class='widget_links'>";
-                            echo "<li><a class='button' target='".$target."' href='".$instance['link']."'>".$instance['linktext']."</a></li>";
-                        echo "</ul>";
+                        echo "<li><a class='button' target='".$target."' href='".$instance['link']."'>".$instance['linktext']."</a></li>";
                     }
+                    if(!empty($instance['link2']) && !empty($instance['linktext2'])) {
+                        $target = $this->getLinkTarget($instance['link2'], home_url());
+                        echo "<li><a class='button' target='".$target."' href='".$instance['link2']."'>".$instance['linktext2']."</a></li>";
+                    }
+                    echo "</ul>";
 
                     echo "</div>";
                 echo "</article>";
@@ -129,69 +135,45 @@ class coenv_base_content extends WP_Widget {
  */
     public function form( $instance ) {
 
-        global $widget_error;
-        if ( is_wp_error( $widget_error) ) {
-            foreach ( $widget_error->get_error_messages() as $error ) {
-                echo '<div class="notice notice-error is-dismissible">';
-                echo '<strong>ERROR</strong>: ';
-                echo $error . '<br/>';
-                echo '</div>';
-            }
-        }
-        unset($widget_error);
-
-        $title='';
-        if(isset($instance['title'])) {
-            $title = $instance['title'];
+        if ( $instance['error'] ) {
+            echo '<div class="notice notice-error is-dismissible">';
+            echo '<strong>ERROR</strong>: ';
+            echo $instance['error'] . '<br/>';
+            echo '</div>';
         }
 
-        $image = '';
-        if(isset($instance['image'])) {
-            $image = $instance['image'];
-        }
-
-        $imageID = '';
-        if(isset($instance['imageID'])) {
-            $imageID = $instance['imageID'];
-        }
-
-        $content = '';
-        if(isset($instance['content'])) {
-            $content = $instance['content'];
-        }
-
-        $link = '';
-        if(isset($instance['link'])) {
-            $link = $instance['link'];
-        }
-
-        $linktext = '';
-        if(isset($instance['linktext'])) {
-            $linktext = $instance['linktext'];
-        }
         ?>
         <p>
             <label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?= (isset($instance['title']) ? $instance['title'] : '') ?>" />
         </p>
         <p>
             <label for="<?php echo $this->get_field_name( 'content' ); ?>"><?php _e( 'Content:' ); ?></label>
-            <textarea class="widefat" id="<?php echo $this->get_field_id( 'content' ); ?>" name="<?php echo $this->get_field_name( 'content' ); ?>" type="textarea"><?php echo esc_attr( $content ); ?></textarea>
+            <textarea class="widefat" id="<?php echo $this->get_field_id( 'content' ); ?>" name="<?php echo $this->get_field_name( 'content' ); ?>" type="textarea"><?= (isset($instance['content']) ? $instance['content'] : '') ?></textarea>
+        </p>
+       <p>
+            <label for="<?php echo $this->get_field_name( 'link' ); ?>"><?php _e( 'Primary Link URL:' ); ?></label>
+            <input class="widefat" placeholder="http(s)://" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?= (isset($instance['link']) ? esc_attr( $instance['link']) : ''); ?>" />
         </p>
         <p>
-            <label for="<?php echo $this->get_field_name( 'link' ); ?>"><?php _e( 'Link URL:' ); ?></label>
-            <input class="widefat" placeholder="http(s)://" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?php echo esc_attr( $link ); ?>" />
+            <label for="<?php echo $this->get_field_name( 'linktext' ); ?>"><?php _e( 'Primary Link Button Text:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'linktext' ); ?>" name="<?php echo $this->get_field_name( 'linktext' ); ?>" type="text" value="<?= (isset($instance['linktext']) ? $instance['linktext'] : ''); ?>" />
         </p>
         <p>
-            <label for="<?php echo $this->get_field_name( 'linktext' ); ?>"><?php _e( 'Link Button Text:' ); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id( 'linktext' ); ?>" name="<?php echo $this->get_field_name( 'linktext' ); ?>" type="text" value="<?php echo $linktext; ?>" />
+            <label for="<?php echo $this->get_field_name( 'link2' ); ?>"><?php _e( 'Secondary Link URL:' ); ?></label>
+            <input class="widefat" placeholder="http(s)://" id="<?php echo $this->get_field_id( 'link2' ); ?>" name="<?php echo $this->get_field_name( 'link2' ); ?>" type="text" value="<?= (isset($instance['link2']) ? esc_attr( $instance['link2']) : '' ); ?>" />
         </p>
+        <p>
+            <label for="<?php echo $this->get_field_name( 'linktext2' ); ?>"><?php _e( 'Secondary Link Button Text:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'linktext2' ); ?>" name="<?php echo $this->get_field_name( 'linktext2' ); ?>" type="text" value="<?= (isset($instance['linktext2']) ? $instance['linktext2'] : ''); ?>" />
+        </p> 
 
         <p>
             <label for="<?php echo $this->get_field_name( 'image' ); ?>"><?php _e( 'Image:' ); ?></label>
-            <img id="coenv_media_image<?php echo $this->number; ?>" style="width:100%" <?=(empty($image) ? 'hidden' : '')?> src="<?php echo $image; ?>" />
-            <input name="<?php echo $this->get_field_name( 'image' ); ?>" id="coenv_media_url<?php echo $this->number; ?>" readonly type="text" size="36"  value="<?php echo esc_url( $image ); ?>" />
-            <input name="<?php echo $this->get_field_name( 'imageID' ); ?>" id="coenv_media_id<?php echo $this->number; ?>" hidden type="text" size="36"  value="<?php echo $imageid; ?>" />
+            <br />
+            <img id="coenv_media_image<?php echo $this->number; ?>" style="width:100%" <?=(empty($instance['image']) ? 'hidden' : '')?> src="<?= (isset($instance['image']) ? $instance['image'] : '') ?>" />
+            <input name="<?php echo $this->get_field_name( 'image' ); ?>" id="coenv_media_url<?php echo $this->number; ?>" readonly type="text" size="36"  value="<?= (isset($instance['image']) ? esc_url( $instance['image'] ) : ''); ?>" />
+            <input name="<?php echo $this->get_field_name( 'imageID' ); ?>" id="coenv_media_id<?php echo $this->number; ?>" hidden type="text" size="36"  value="<?= (isset($instance['imageID']) ? $instance['imageID'] : ''); ?>" />
             <input class="custom_media_upload button button-primary" id="<?php echo $this->number; ?>" type="button" value="Upload/Select Image" />
             <input class="remove_custom_media button" type="button" id="<?php echo $this->number; ?>" value="Remove Image" />
         </p>
@@ -209,41 +191,31 @@ class coenv_base_content extends WP_Widget {
   * @return array Updated safe values to be saved.
   */
     public function update( $new_instance, $old_instance ) {
-        global $widget_error;
-        $widget_error = '';
 
-        $instance = array();
-        if(!empty($new_instance['title'])) {
-            $instance['title'] = strip_tags( $new_instance['title'] );
-        } else {
-            $widget_error = new WP_Error;
-            $widget_error->add('invalid_title', 'You must include a title');
+        $old_instance['error'] = '';
+
+        if(!$new_instance['title']) {
+            $old_instance['error'] = "This widget must have a title.";
         }
 
-        $instance['content'] = $new_instance['content'];
-
-        if(!filter_var($new_instance['link'], FILTER_VALIDATE_URL) === false) {
-            $instance['link'] = $new_instance['link'];
-        } else {
-            $instance['link'] = "http://" + $new_instance['link'];
-            if(filter_var($instance['link'], FILTER_VALIDATE_URL) === false) {
-                $widget_error = new WP_Error;
-                $widget_error->add('invalid_link', 'Link must be a valid url');
+        if($new_instance['link'] != '') {
+            if(!$this->validateLink($new_instance['link'])) {
+                $old_instance['error'] = "Link 1 URL was invalid. HINT: You must include http://";
             }
         }
 
-        $instance['linktext'] = $new_instance['linktext'];
+        if($new_instance['link2'] != ''){
+            if(!$this->validateLink($new_instance['link2'])) {
+                $old_instance['error'] = "Link 2 URL was invalid. HINT: You must include http://";
+            }
+        }
 
-        $instance['image'] = $new_instance['image'];
-        $instance['imageID'] = $new_instance['imageID'];
-
-        if ( is_wp_error( $widget_error ) ) {
-            return false;
+        if(!$old_instance['error']) {
+            return $new_instance;
         } else {
-            return $instance;
+            return $old_instance;
         }
     }
-
 }
 function register_coenv_base_content() {
     register_widget( 'coenv_base_content' );
