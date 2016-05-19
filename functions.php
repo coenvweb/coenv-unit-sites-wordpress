623b5f57428f907c8cebaade9798ad98d2a6ed36
drewcoll <drewcoll@gmail.com>
2016-05-18 17:07:41 -0700

    accessible category select

--- functions.php
+++ functions.php
@@ -226,7 +226,8 @@ $cats_args  = array(
 );
 $cats = get_categories($cats_args);
 	if ($cats) {
-		echo '<select name="select-category" class="select-category">';
+        echo '<label for="select-category" class="visuallyhidden">Research Area Filter</label>';
+		echo '<select name="select-category" class="select-category" id="select-category">';
 		echo '<option class="level-0" value="' . strtok($_SERVER['REQUEST_URI'],'?') . '">All ' . $tax_str . '</option>';
 		foreach($cats as $cat) { 
 			$selected = $cat->slug == $tax_value ? ' selected="selected"' : '';
