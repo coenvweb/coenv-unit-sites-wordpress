16ed55bddfe8a02ed016781931c29b472a690867
Cole Bessee <cbessee@uw.edu>
2016-06-08 15:08:18 -0700

    Cache twitter results in db for 15 mins to avoid api rate and improve performance

--- library/twitter.php
+++ library/twitter.php
@@ -12,11 +12,14 @@ function getUserTimeline() {
     require_once "twitterSecret.php";
 
 
-    $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
-
-    $tweets = $connection->get($_REQUEST['api'], ["screen_name" => $_REQUEST['screen_name'], "count" => $_REQUEST['count'], "exclude_replies" => true, "include_rts" => true]);
-
-    $tweetArray = json_encode($tweets);
+    if( false === ( $tweetArray = get_transient('twitter_feed') ) ) {
+        $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
+        $tweets = $connection->get($_REQUEST['api'], ["screen_name" => $_REQUEST['screen_name'], "count" => $_REQUEST['count'], "exclude_replies" => true, "include_rts" => true]);
+        $tweetArray = json_encode($tweets);
+        set_transient('twitter_feed', $tweetArray, 900);
+    } else {
+        $tweetArray = get_transient('twitter_feed');
+    }
 
     echo $tweetArray;
     die();
