9f802a4853f191f83308651e15a67d3327980578
Cole Bessee <cbessee@uw.edu>
2016-06-15 08:44:39 -0700

    Show a third news column on homepage if no events exist in trumba

--- templates/homepage.php
+++ templates/homepage.php
@@ -190,7 +190,7 @@ $posted = array();
 		</div>
 		<div class="home-news-section columns large-12 clearfix">
 			<div class="row">
-				<div class="columns small-12 medium-4 news-column">
+			<div class="columns small-12 medium-4 news-column">
 				<?php
 				while ( $wp_query->have_posts() ) :
 					// Get field vars
@@ -235,7 +235,7 @@ $posted = array();
 				<?php endwhile;?>
 				<?php wp_reset_postdata(); ?>
 
-				</div>
+				</div>	
 				<div class="columns small-12 medium-4 news-column">
 
 				<?php
@@ -348,18 +348,76 @@ $posted = array();
 				<?php wp_reset_postdata(); ?>
 
 				</div>
-				<div class="columns small-12 medium-4 news-column">
 				<?php
-					?>
-					<?php //if ( !empty( $events ) ) { ?>
+                    //if no events, then add a third news column
+                    $events_xml = fetch_feed( 'https://www.trumba.com/calendars/sea_fish.rss?posts_per_page=4' );
+					if ( $events_xml->get_items() == false || $events_xml == '' ) { ?>
+                        <div class="columns small-12 medium-4 news-column">
+                        <?php
+                        $home_col_3_args = array(
+                            'post_type' => 'post',
+                            'category__not_in' => '922',
+                            'posts_per_page' => 2,
+                            'offset' => 2,
+                            'post_status' => 'publish',
+                        );
+                        $wp_query = new WP_Query( $home_col_3_args );
+                        if($wp_query->have_posts()) {
+
+                        while ( $wp_query->have_posts() ) :
+                            // Get field vars
+                            $wp_query->the_post();
+                            $posted[] = get_the_id();
+                            if (get_field('story_link_url')) {
+                                $post_link_url = get_field('story_link_url');
+                                $post_link_target = 'target="_blank"';
+                                $post_link = '<p><a class="button" href="' . $post_link_url . '"' . $post_link_target . '>' . get_field('story_source_name') . '</a></p>';
+                            } else {
+                                $post_link_url = get_the_permalink();
+                                $post_link = '<a class="button left" href="' . $post_link_url . '">Read more</a>';
+                            }
+                            $terms = wp_get_post_terms(get_the_id(), 'category');
+                            if ( !empty($terms) ) {
+                                $terms_list = array();
+                                foreach ( $terms as &$term ) {
+                                    if ( $term->slug != 'uncategorized' ) {
+                                        $terms_list[] = '<li><a href="/news-events/?tax=category&amp;term=' . $term->slug . '">' . $term->name . '</a></li>';
+                                    }
+                                }
+                            } else {
+                                $terms_list = '';
+                            }
+                            ?>
+
+                            <div class="small-news col-1">
+                                <h3><a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a></h3>
+
+                                <span class="show-for-medium-up"><?php strip_tags(the_advanced_excerpt('length=30&finish=sentence'),''); ?></span>
+
+                                <div class="post-meta clearfix row show-for-medium-up">
+                                    <time class="article__time columns small-12 medium-5 left" datetime="<?php echo get_the_date('Y-m-d h:i:s'); ?>"><?php echo get_the_date( 'M j, Y' ); ?></time>
+                                    <?php if ( !empty($terms ) ) { ?> 
+                                    <ul class="terms right columns small-12 medium-7 right text-right">
+                                        <?php echo implode(", ", $terms_list); ?>
+                                    </ul>
+                                    <?php } ?>
+                                </div>
+                            </div>
+
+                        <?php endwhile;?>
+                        <?php wp_reset_postdata(); ?>
+                        <?php } ?>
+                        </div>
+                    <?php } else { ?>
 					<section class="events clearfix">
 						<header>
 							<h3><a href="/news-events/events/">Events</a></h3>
 						</header>
-						<?php the_widget('CoEnv_Widget_Events', 'feed_url=https://www.trumba.com/calendars/sea_fish.rss&posts_per_page=4&'); ?>
+						<?php the_widget('CoEnv_Widget_Events', 'feed_url=https://www.trumba.com/calendars/sea_fish.rss?posts_per_page=4'); ?>
 						<footer><a class="more-events right" href="/news-events/events/">More Events</a></footer>
 					</section>
 				</div>
+                    <?php } ?>
 				
 				<?php endif; ?>
 
