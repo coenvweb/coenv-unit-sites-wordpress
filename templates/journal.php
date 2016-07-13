94c55f6e577d62979d90164770a0d2a7149fa32e
Cole Bessee <cbessee@uw.edu>
2016-07-13 14:15:42 -0700

    Remove double call to before content on journal page

--- templates/journal.php
+++ templates/journal.php
@@ -20,7 +20,6 @@ $journal_cover = get_field('journal_cover', 'option');
     <?php dynamic_sidebar("before-content"); ?>
     <?php while (have_posts()) : the_post(); ?>
         <article <?php post_class() ?> id="post-<?php the_ID(); ?>" class="template-page">
-            <?php do_action('foundationPress_page_before_entry_content'); ?>
             <div class="entry-content">
                 <?php the_content(); ?>
             </div>
