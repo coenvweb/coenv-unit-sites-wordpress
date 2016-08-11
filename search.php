feebfeffe1de6b53022329f3b86ea04c1d93e6db
Cole Bessee <cbessee@uw.edu>
2016-08-11 10:26:39 -0700

    update layout of search and 404 pages

--- search.php
+++ search.php
@@ -1,19 +1,34 @@
 <?php get_header(); ?>
 <div class="row">
-	<div class="small-12 medium-8 columns" role="main">
+	<div class="small-12 columns" role="main">
 	
-		<?php do_action('foundationPress_before_content'); ?>
-	
-		<h2><?php _e('Search Results for', 'FoundationPress'); ?> "<?php echo get_search_query(); ?>"</h2>
-	
-	<?php if ( have_posts() ) : ?>
-	
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-		<?php endwhile; ?>
+    <?php if ( have_posts() ) : ?>
+        <div class="search-filter">
+
+        <?php do_action('foundationPress_before_content'); ?>
+
+        <h2><?php _e('Results for', 'FoundationPress'); ?> "<?php echo get_search_query(); ?>"</h3>
+
+        <?php get_search_form(); ?>
+        </div>
+
+        <?php while ( have_posts() ) : the_post(); ?>
+            <a href="<?php echo the_permalink(); ?>"><h3><?php echo the_title(); ?></h3></a>
+            <p>
+            <?php
+            $teaser_limited = get_the_excerpt();
+            $teaser_limited = strip_tags($teaser_limited);
+            $teaser_limited = trim($teaser_limited, '!,?.&nbsp;');
+            $teaser_limited = substr($teaser_limited, 0, -2);
+            if ($teaser_limited) {
+                echo $teaser_limited . '...';
+            }
+            ?>
+            </p>
+        <?php endwhile; ?>
 		
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
+    <?php else : ?>
+        <?php get_template_part( 'content', 'none' ); ?>
 		
 	<?php endif;?>
 	
@@ -30,6 +45,6 @@
 	<?php do_action('foundationPress_after_content'); ?>
 
 	</div>
-	<?php get_sidebar(); ?>
-		
+	</div>
+</div>
 <?php get_footer(); ?>
