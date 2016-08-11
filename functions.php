648b40154bb233f8aa56263b8c1e1c5546035cdd
Cole Bessee <cbessee@uw.edu>
2016-08-11 09:30:17 -0700

    Fix misspellings from site-improve

--- functions.php
+++ functions.php
@@ -236,7 +236,7 @@ $cats = get_categories($cats_args);
 	if ($cats) {
         echo '<label class="visuallyhidden" for="select-category">Choose a category</label>';
 		echo '<select name="select-category" class="select-category" id="select-category">';
-		echo '<option class="level-0" value="' . strtok($_SERVER['REQUEST_URI'],'?') . '">Chooose a category</option>';
+		echo '<option class="level-0" value="' . strtok($_SERVER['REQUEST_URI'],'?') . '">Choose a category</option>';
 		echo '<ul class="select-category small-block-grid-3">';
  		foreach($cats as $cat) { 
 			$selected = $cat->slug == $tax_value ? ' selected="selected"' : '';
