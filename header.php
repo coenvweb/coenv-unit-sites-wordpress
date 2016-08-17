e3dd087be0a068821adaf6e6d3efab86844b6282
Cole Bessee <cbessee@uw.edu>
2016-08-17 11:51:42 -0700

    Add backgrounds for learn and community homepage buttons

--- header.php
+++ header.php
@@ -21,7 +21,6 @@
       echo wp_title( ' | ', 'false', 'right' ); bloginfo( 'name' );
     } ?></title>
     
-  <script async src="//www.washington.edu/static/alert.min.js" type="text/javascript"></script>
 
             <!--[if IE 9]><link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ; ?>/css/appie-blessed1dev.css" /><![endif]-->
           <!--[if IE 9]><link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ; ?>/css/appie.css" /><![endif]-->
@@ -45,6 +44,7 @@
     <meta name="msapplication-TileColor" content="#4b2e84">
     <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri() ?>/assets/img/ms-icon-144x144.png">
     <meta name="theme-color" content="#4b2e84">
+    <script async src="//www.washington.edu/static/alert.min.js" type="text/javascript"></script>
   
     <?php wp_head(); ?>
       
