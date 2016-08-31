306539869d2b0c65838025942783f525a65e0d33
Cole Bessee <cbessee@uw.edu>
2016-08-31 09:47:03 -0700

    Accessibility updates to footer and mobile menu

--- searchform.php
+++ searchform.php
@@ -1,7 +1,6 @@
 <form role="search" method="get" class="search-form Form--inline" action="<?php echo home_url( '/' ); ?>">
   <div class="field-wrap">
-	<label for="s">Search Field</label>
-    <input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" placeholder="Search this site" />
+    <input type="text" title="search" aria-label="Search" value="<?php echo get_search_query() ?>" name="s" id="s" placeholder="Search this site" />
     <button type="submit"><i class="fi-magnifying-glass"></i><span>Search</span></button>
   </div>
 </form>
