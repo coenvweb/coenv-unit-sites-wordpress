a7575a09b82ac5daf693c32c8466deaa8c14a5e8
drewcoll <drewcoll@gmail.com>
2016-10-10 14:53:55 -0700

    footer uwbg link

--- footer.php
+++ footer.php
@@ -53,7 +53,7 @@
                         <li><a target="_blank" href="http://jisao.washington.edu/">Joint Institute for the Study of the Atmosphere and Ocean</a></li>
                         <li><a target="_blank" href="http://uwpcc.washington.edu">Program on Climate Change</a></li>
                         <li><a target="_blank" href="http://qrc.uw.edu">Quaternary Research Center</a></li>
-                        <li><a target="_blank" href="http://depts.washington.edu/uwbg/">UW Botanic Gardens</a></li>
+                        <li><a target="_blank" href="https://botanicgardens.uw.edu/">UW Botanic Gardens</a></li>
                         <li><a target="_blank" href="http://www.waspacegrant.org/">Washington NASA Space Grant</a></li>
                         <li><a target="_blank" href="http://wsg.washington.edu/">Washington Sea Grant</a></li>
                     </ul>
