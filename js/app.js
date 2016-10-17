dfd78102fd43b6e6d3d065d2a2f52d73f2cd380e
drewcoll <drewcoll@gmail.com>
2016-10-17 14:54:09 -0700

    uw alert banner x

--- js/app.js
+++ js/app.js
@@ -345,7 +345,35 @@ $.fn.blogHeader = function () {
 
 
 
-;jQuery(function ($) {
+;/**
+ * Close UW Alert
+ */
+
+jQuery(document).ready(function() {
+  closeUWAlert();
+});
+
+function closeUWAlert () {
+  if($('#uwalert-alert-message').is(':hidden')){ //if the container is visible on the page
+    if ($('#uwalert-alert-message')){
+        $('#uwalert-alert-header').append('<div class="button right" id="closer">X</div>');
+        var alertHeading = $('#uwalert-alert-header')[0];
+        $('#closer').on('click', function(e){
+            $('#uwalert-alert-message').removeClass('please-unhide');
+            $('#uwalert-alert-message').hide();
+            localStorage.clicked = alertHeading.innerHTML;
+        });
+        if(localStorage.clicked === alertHeading.innerHTML){
+            console.log('UW Alert is hidden ' + localStorage.clicked);
+            $('#uwalert-alert-message').hide();
+        } else {
+            $('#uwalert-alert-message').addClass('please-unhide');
+        }
+    }
+  } else {
+    setTimeout(closeUWAlert, 50); //wait 50 ms, then try again
+  }
+};jQuery(function ($) {
 	'use strict';
 	$( ".single-faculty li.page-item-35" ).removeClass( "active" );
 	$( ".single-faculty li.page-item-31" ).addClass( "active" );
