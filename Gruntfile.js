5f41c6b86659f3b33465a78994ff98bf62ce2727
drewcoll <drewcoll@gmail.com>
2016-10-17 14:54:19 -0700

    uw alert banner x

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,6 +63,7 @@ module.exports = function(grunt) {
 		      'js/slick.js/slick/slick.min.js',
 		      'js/share.js',
 		      'js/main.js',
+            './js/alert-closer.js',
           'js/wp-menufix.js',
         ],
 
