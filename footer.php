4f3fcb63a3b17ec862c841c128040033f9c2b8b5
Cole Bessee <cbessee@uw.edu>
2016-11-30 10:25:48 -0800

    add college logo and link to site footer at mobile

--- footer.php
+++ footer.php
@@ -6,6 +6,9 @@
             <div class="smea-logo left">
                 <?php get_template_part('assets/img/icons/inline', 'smea-logo.svg'); ?>
             </div>
+			<div class="site-footer__header">
+				<h2 class="show-for-small-only" id="logo"><a href="http://coenv.washington.edu/" rel="home" title="UW College of the Environment"><img alt="College of the Environment Logo" src="<?php bloginfo('template_directory'); ?>/assets/img/uw-footer.svg" width="350" ></a></h2>
+			</div>
             <div class="site-footer__header">
                 <h2><?php bloginfo('name') ?></h2>
             </div>
