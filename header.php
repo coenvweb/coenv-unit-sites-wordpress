75eeb0bef0a71a6b517995c3c2697f81e3139d82
Cole Bessee <cbessee@uw.edu>
2017-01-09 12:30:13 -0800

    add outbound tracking links

--- header.php
+++ header.php
@@ -50,12 +50,7 @@
     $coenv_host_dev = strpos($coenv_host,'.dev');
     if (!$coenv_host_dev)  { ?>
     <script>
-	  var trackOutboundLink = function(url) {
-		 ga('send', 'event', 'outbound', 'click', url, {
-			'transport': 'beacon',
-			'hitCallback': function(){document.location = url;}
-		 });
-	  }
+	  
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -63,6 +58,12 @@
 
       ga('create', 'UA-67450612-1', 'auto');
       ga('send', 'pageview');
+      var trackOutboundLink = function(url) {
+		 ga('send', 'event', 'outbound', 'click', url, {
+			'transport': 'beacon',
+			'hitCallback': function(){document.location = url;}
+		 });
+	  }
     </script>
     <?php } ?>
   </head>
