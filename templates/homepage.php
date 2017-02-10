5f3f4a0db0d8ce474637560e4637a6957454d61f
Cole Bessee <cbessee@uw.edu>
2017-02-10 13:09:57 -0800

    shorten a bit more

--- templates/homepage.php
+++ templates/homepage.php
@@ -200,7 +200,7 @@ wp_reset_postdata();
 
 							<?php
 								echo '<h3><a href="' . $post_link_url . '"' . $post_link_target . '>' . get_the_title() . '</a></h3>';
-                                echo the_advanced_excerpt('length=30&finish=sentence');
+                                echo the_advanced_excerpt('length=20&finish=sentence');
 								echo $post_link;
 							'</div>';
                             ?>
