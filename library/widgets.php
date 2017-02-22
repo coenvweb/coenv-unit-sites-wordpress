e9e36333db193e4a4e99185d5fa9ef703f8ee497
Cole Bessee <cbessee@uw.edu>
2017-02-22 14:09:56 -0800

    remove ssl from events fetch for now

--- library/widgets.php
+++ library/widgets.php
@@ -315,7 +315,13 @@ class CoEnv_Widget_Events extends WP_Widget {
     // get cached XML from WP transient API
     $events_xml = get_transient( 'trumba_events_xml' );
     if ( $events_xml === false || $events_xml === '' ) {
-      $events_xml = file_get_contents( $feed_url );
+	  $ctx = stream_context_create(array('http'=>
+            array(
+                'timeout' => 3,  //1200 Seconds is 20 Minutes
+            ),  
+            'ssl' => array('verify_peer' => false, 'verify_peer_name' => false),
+        ));
+      $events_xml = file_get_contents( $feed_url, false, $ctx);
       set_transient( 'trumba_events_xml', $events_xml, 1 * MINUTE_IN_SECONDS );
     }
     
