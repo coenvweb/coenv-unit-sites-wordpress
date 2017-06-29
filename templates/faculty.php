76d7877c3a084b9b23f8cb437494a87d41933d3b
drewcoll <drewcoll@gmail.com>
2017-06-29 16:46:36 -0700

    faculty page

--- templates/faculty.php
+++ templates/faculty.php
@@ -3,36 +3,15 @@
 Template Name: Faculty Index
 */
 
-/*
- * Query variables
- */
-
-//Categories
-$coenv_cat_1 = urlencode(htmlentities($_GET['tax']));
-$coenv_cat_term_1 = urlencode(htmlentities($_GET['term']));
-$coenv_cat_term_1_arr = get_term_by('slug',$coenv_cat_term_1,$coenv_cat_1);
-$coenv_cat_term_1_val = $coenv_cat_term_1_arr->name;
 ?>
 
 <?php get_header(); ?>
 <div class="row">
 
-	<div class="small-12 medium-8 columns" role="main">
+	<div class="small-12 medium-8 columns right" role="main">
         <div class="entry-content">
-        	<div class="row filters">
-				<div class=" large-6 columns" data-url="<?php $_SERVER['REQUEST_URI']; ?>" data-cat="blog_category">
-				<?php coenv_base_cat_filter('research_areas', $coenv_cat_term_1); // Category filter ?>
-				</div>
-				<div class="share columns large-6" data-article-id="<?php the_ID(); ?>" data-article-title="<?php echo get_the_title(); ?>"
-		data-article-shortlink="<?php echo wp_get_shortlink(); ?>"
-		data-article-permalink="<?php echo the_permalink(); ?>"><a href="#"><i class="fi-share"></i>Share</a></div>
-	</div>
-
 <?php
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
-$temp = $wp_query;
-$wp_query = null;
 $wp_query = new WP_Query();
 $wp_query->query;
 
@@ -43,8 +22,6 @@ $query_args = array(
 	'post_type'	=> 'faculty',
 	'post_status' => 'publish',
 	'posts_per_page' => -1,
-	'taxonomy' => 'research_areas',
-	'term' => $fac_cat->slug,
 	'meta_key' => 'last_name',
 	'orderby' => 'meta_value',
 	'order' => 'ASC',
@@ -57,23 +34,12 @@ $query_args = array(
 	),
 );
 
-// Category filter
-if($coenv_cat_1 && $coenv_cat_term_1) :
-	$query_args['taxonomy'] = $coenv_cat_1;
-	$query_args['term'] = $coenv_cat_term_1;
-endif;
 $wp_query = new WP_Query( $query_args );
 
 ?>
 	<?php if ($wp_query->have_posts()): ?>
 	<div class="faculty-list-teach clearfix">
 
-<?php if ($coenv_cat_1): // Category filter ?>
-		<div class="panel">
-			<div class="left"><?php echo $wp_query->found_posts; ?> faculty working in <strong><?php echo $coenv_cat_term_1_val; ?></strong></div>
-			<div class="right"><a href="/research/publications/">all posts &raquo;</a></div>
-		</div>
-	<?php endif; ?>
 		<?php
 		# The Loop
 		while ( $wp_query->have_posts() ) :
@@ -97,18 +63,6 @@ $wp_query = new WP_Query( $query_args );
 		echo '</div>';
 		endwhile;
 		?>
-				<div class="pager">
-					<?php /* Display navigation to next/previous pages when applicable */ ?>
-	<?php if ( function_exists('FoundationPress_pagination') ) { FoundationPress_pagination(); } else if ( is_paged() ) { ?>
-		<nav id="post-nav">
-			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'FoundationPress' ) ); ?></div>
-			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'FoundationPress' ) ); ?></div>
-		</nav>
-	<?php } ?>
-  </div>
-		
-
-	</div>
 	<?php endif; ?>
         </div>
 	<?php if ( is_active_sidebar( 'after-content' ) ) : ?>
@@ -117,7 +71,6 @@ $wp_query = new WP_Query( $query_args );
 	<?php dynamic_sidebar("after-content"); ?>
 	</ul>
 	<?php endif; ?>
-	<a href="#" class="back-to-top">Back to Top</a>
 	<?php do_action('foundationPress_after_content'); ?>
 	</div>
 	    <?php wp_reset_postdata(); wp_reset_query(); //roll back query vars to as per the request ?>
