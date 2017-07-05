c0b3c21823a00da62d2a6e35a920a07a331f3267
Cole Bessee <cbessee@uw.edu>
2017-07-05 14:59:31 -0700

    update footer links

--- footer.php
+++ footer.php
@@ -47,10 +47,10 @@
                         <li><a target="_blank" href="http://www.sefs.washington.edu/">Environmental and Forest Sciences</a></li>
                         <li><a target="_blank" href="https://smea.uw.edu">Marine and Environmental Affairs</a></li>
                         <li><a target="_blank" href="http://www.ocean.washington.edu/">Oceanography</a></li>
-                        <li><a target="_blank" href="http://depts.washington.edu/poeweb/">Program on the Environment</a></li>
+                        <li><a target="_blank" href="http://envstudies.uw.edu">Program on the Environment</a></li>
                         <li><a target="_blank" href="https://earthlab.uw.edu/">EarthLab</a></li>
-                        <li><a target="_blank" href="http://cses.washington.edu/cig/">Climate Impacts Group</a></li>
-                        <li><a target="_blank" href="http://depts.washington.edu/fhl/">Friday Harbor Laboratories</a></li>
+                        <li><a target="_blank" href="http://cig.uw.edu">Climate Impacts Group</a></li>
+                        <li><a target="_blank" href="http://fhl.uw.edu/">Friday Harbor Laboratories</a></li>
                         <li><a target="_blank" href="http://jisao.washington.edu/">Joint Institute for the Study of the Atmosphere and Ocean</a></li>
                         <li><a target="_blank" href="http://pcc.uw.edu/">Program on Climate Change</a></li>
                         <li><a target="_blank" href="http://depts.washington.edu/qrc/">Quaternary Research Center</a></li>
