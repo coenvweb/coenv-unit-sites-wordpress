0c5db12cb8681e698353550937508d248fdfce05
Cole Bessee <cbessee@uw.edu>
2018-04-11 11:29:53 -0700

    remove debug statement

--- library/shortcodes.php
+++ library/shortcodes.php
@@ -4,7 +4,6 @@ function datatable_func( $atts, $content = null ) {
 	$dt = shortcode_atts( array(
         'search' => 0,
     ), $atts );
-	echo var_dump($dt);
     return '<div class="datatable '.($dt['search'] ? 'search' : 'no-search').'">' . $content . '</div>';
 }
 add_shortcode( 'datatable', 'datatable_func' );
