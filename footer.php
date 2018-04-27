489285cb8244b723bdf9a6a33e6bc176f1711537
Cole Bessee <cbessee@uw.edu>
2018-04-27 11:00:34 -0700

    qerm and alpha unit footers

--- footer.php
+++ footer.php
@@ -9,66 +9,62 @@
                     	<a href="/"><img src="<?php bloginfo('template_directory'); ?>/assets/img/logo-color.png" alt="Climate Impacts Group" /></a>
                 	</div>
                 	<div class="unit-info left columns large-9">
-					<div class="site-footer__header">
-						<h2 class="show-for-small-only" id="logo"><a href="http://coenv.washington.edu/" rel="home" title="UW College of the Environment"><img alt="College of the Environment Logo" src="<?php bloginfo('template_directory'); ?>/assets/img/uw-footer.svg" width="350" ></a></h2>
-					</div>
-					<h2><?php bloginfo('name') ?></h2>
-					<div class="unit-contact">
-						<?php if (get_field('mail_address', 'option')) { ?><p><a href="http://maps.google.com/?q=<?php echo get_field('mail_address', 'option'); ?>" title="Google Maps link"><?php echo get_field('mail_address', 'option'); ?></a></p><?php } ?>
-						<?php if (get_field('public_email_address', 'option')) { ?><p><a href="mailto:<?php echo antispambot(get_field('public_email_address', 'option')); ?>" title="Send us an Email"><?php echo antispambot(get_field('public_email_address', 'option')); } ?></a>
-						<?php if (get_field('phone', 'option')) { ?> | <?php echo get_field('phone', 'option'); ?></p><?php } ?>
-					</div>
-					<div class="footer__info">
-						<?php get_search_form() ?>
-						<div class="social-buttons">
-							<?php if (get_field('facebook', 'option')) { ?>
-							<a class="facebook button" href="<?php echo get_field('facebook', 'option'); ?>" title="Join us on Facebook">
-								<i class="fi-social-facebook"></i>
-							</a><?php } ?>
-							<?php if (get_field('twitter', 'option')) { ?>
-							<a class="twitter button" href="<?php echo 'http://twitter.com/' . get_field('twitter', 'option'); ?>" data-site-twitter="<?php echo get_field('twitter', 'option'); ?>" title="Join us on Twitter">
-								<i class="fi-social-twitter"></i>
-							</a><?php } ?>
-							<?php if (get_field('youtube', 'option')) { ?>
-							<a class="youtube button" href="<?php echo get_field('youtube', 'option'); ?>" title="Join us on YouTube">
-								<i class="fi-social-youtube"></i>
-							</a><?php } ?>
-						</div>
-					</div>
-
-				</div>
-
-
-				</div>
-			</div>
-			<div class="medium-6 columns right">
-				<nav class="footer-nav">
-					<header class="site-footer__header">
-						<h2 id="logo"><a href="http://coenv.washington.edu/" rel="home" title="UW College of the Environment"><img alt="College of the Environment Logo" src="<?php bloginfo('template_directory'); ?>/assets/img/uw-footer.svg" width="350" ></a></h2>
-					</header>
-						<ul class="menu-footer-units">
-							<li><a target="_blank" href="http://fish.washington.edu/">Aquatic and Fishery Sciences</a></li>
-							<li><a target="_blank" href="http://www.atmos.washington.edu/">Atmospheric Sciences</a></li>
-							<li><a target="_blank" href="http://www.ess.washington.edu/">Earth and Space Sciences</a></li>
-							<li><a target="_blank" href="http://www.sefs.washington.edu/">Environmental and Forest Sciences</a></li>
-							<li><a target="_blank" href="https://smea.uw.edu">Marine and Environmental Affairs</a></li>
-							<li><a target="_blank" href="http://www.ocean.washington.edu/">Oceanography</a></li>
-							<li><a target="_blank" href="http://envstudies.uw.edu">Program on the Environment</a></li>
-							<li><a target="_blank" href="https://earthlab.uw.edu">EarthLab</a></li>
-							<li><a target="_blank" href="https://cig.uw.edu">Climate Impacts Group</a></li>
-							<li><a target="_blank" href="http://fhl.uw.edu">Friday Harbor Laboratories</a></li>
-							<li><a target="_blank" href="http://jisao.washington.edu/">Joint Institute for the Study of the Atmosphere and Ocean</a></li>
-                            <li><a target="_blank" href="http://pcc.uw.edu">Program on Climate Change</a></li>
-                            <li><a target="_blank" href="http://qrc.uw.edu">Quaternary Research Center</a></li>
-							<li><a target="_blank" href="https://botanicgardens.uw.edu/">UW Botanic Gardens</a></li>
-							<li><a target="_blank" href="http://www.waspacegrant.org/">Washington NASA Space Grant</a></li>
-							<li><a target="_blank" href="http://wsg.washington.edu/">Washington Sea Grant</a></li>
-							<li></li>
-						</ul>
-					</nav>
-				</div>
+                        <div class="site-footer__header">
+                            <h2><?php bloginfo('name') ?></h2>
+                        </div>
+                        <div class="unit-contact">
+                            <?php if (get_option('mail_address')) { ?><p><a href="http://maps.google.com/?q=<?php echo urlencode(get_option('mail_address')); ?>" title="Google Maps link"><?php echo get_option('mail_address'); ?></a></p><?php } ?>
+                            <?php if (get_option('public_email_address')) { ?><p><a href="mailto:<?php echo antispambot(get_option('public_email_address')); ?>" title="Send us an Email"><?php echo antispambot(get_option('public_email_address')); } ?></a>
+                            <?php if (get_option('phone')) { ?> | <?php echo get_option('phone'); ?></p><?php } ?>
+                        </div>
+                        <div class="footer__info">
+                            <?php get_search_form() ?>
+                            <div class="social-buttons">
+                            <?php if (get_option('facebook')) { ?>
+                                <a class="facebook button" href="<?php echo get_option('facebook'); ?>" title="Join us on Facebook" target="_blank">
+                                    <i class="fi-social-facebook"></i><span class="visuallyhidden">Facebook</span>
+                                </a><?php } ?>
+                            <?php if (get_option('twitter')) { ?>
+                                <a class="twitter button" href="<?php echo 'http://twitter.com/' . get_option('twitter'); ?>" data-site-twitter="<?php echo get_option('twitter'); ?>" title="Join us on Twitter" target="_blank">
+                                        <i class="fi-social-twitter"></i><span class="visuallyhidden">Twitter</span>
+                                </a><?php } ?>
+                            <?php if (get_option('youtube')) { ?>
+                                <a class="youtube button" href="<?php echo get_option('youtube'); ?>" title="Join us on YouTube">
+                                        <i class="fi-social-youtube"></i><span class="visuallyhidden">YouTube</span>
+                                </a><?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div class="medium-6 columns right">
+                <nav class="footer-nav">
+                    <div class="site-footer__header">
+                        <h2><a href="http://coenv.washington.edu/" rel="home" title="UW College of the Environment"><img alt="College of the Environment Logo" src="<?php bloginfo('template_directory'); ?>/assets/img/uw-footer.svg" width="350" ><span class="visuallyhidden">University of Washington College of the Environment</span></a></h2>
+                    </div>
+                        <ul class="menu-footer-units">
+                            <li><a target="_blank" href="http://fish.washington.edu/">Aquatic and Fishery Sciences</a></li>
+                            <li><a target="_blank" href="http://www.atmos.washington.edu/">Atmospheric Sciences</a></li>
+                            <li><a target="_blank" href="http://cig.uw.edu">Climate Impacts Group</a></li>
+                            <li><a target="_blank" href="http://www.ess.washington.edu/">Earth and Space Sciences</a></li>
+                            <li><a target="_blank" href="https://earthlab.uw.edu/">EarthLab</a></li>
+                            <li><a target="_blank" href="http://www.sefs.washington.edu/">Environmental and Forest Sciences</a></li>
+                            <li><a target="_blank" href="http://fhl.uw.edu/">Friday Harbor Laboratories</a></li>
+                            <li><a target="_blank" href="http://jisao.washington.edu/">Joint Institute for the Study of the Atmosphere and Ocean</a></li>
+                            <li><a target="_blank" href="https://smea.uw.edu">Marine and Environmental Affairs</a></li>
+                            <li><a target="_blank" href="http://www.ocean.washington.edu/">Oceanography</a></li>
+                            <li><a target="_blank" href="http://pcc.uw.edu/">Program on Climate Change</a></li>
+                            <li><a target="_blank" href="http://envstudies.uw.edu">Program on the Environment</a></li>
+                            <li><a target="_blank" href="http://depts.washington.edu/qerm/">Quantitative Ecology and Resource Management</a></li>
+                            <li><a target="_blank" href="http://depts.washington.edu/qrc/">Quaternary Research Center</a></li>
+                            <li><a target="_blank" href="http://botanicgardens.uw.edu/">UW Botanic Gardens</a></li>
+                            <li><a target="_blank" href="http://www.waspacegrant.org/">Washington NASA Space Grant</a></li>
+                            <li><a target="_blank" href="http://wsg.washington.edu/">Washington Sea Grant</a></li>
+                            <li></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
 			
         <div class="footer-footer">
 <div class="uw-footer">
