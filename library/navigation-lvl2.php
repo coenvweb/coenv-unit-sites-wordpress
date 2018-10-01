388efea6737115ab2e5bcb87eeed24edae261ac9
drewcoll <drewcoll@gmail.com>
2018-10-01 16:16:33 -0700

    tribal vulnerability resources subnav switch

--- library/navigation-lvl2.php
+++ library/navigation-lvl2.php
@@ -10,6 +10,9 @@ function coenv_base_hierarchical_submenu($postid) {
     if ($post->post_parent && $post->ancestors) {
         $top_post = get_post(end($post->ancestors));
     }
+    if ($post->ID == '15466' || in_array(15466, $post->ancestors) || $post->ID == '14250' || in_array(14250, $post->ancestors)) { //for tribal vulnerability pages
+         return special_tvar_coenv_base_hierarchical_submenu_get_children($top_post, $post);
+    }
     // Always start traversing from the top of the tree
     return coenv_base_hierarchical_submenu_get_children($top_post, $post);
 }
@@ -36,9 +39,44 @@ function coenv_base_hierarchical_submenu_get_children($post, $current_page) {
         foreach ($children as $child) {
             // If the child is the viewed page or one of its ancestors, highlight it
             if (in_array($child->ID, get_post_ancestors($current_page)) || ($child->ID == $current_page->ID)) {
-                $menu .= '<li class="active"><a href="' . get_permalink($child) . '" class="active">' . $child->post_title . '</a>';
+                $menu .= '<li class="active"><a href="' . get_permalink($child) . '" class="active">' . $child->post_title . '</a>';  
             } else {
                 $menu .= '<li><a href="' . get_permalink($child) . '">' . $child->post_title . '</a>';
+            }
+            // If the page has children and is the viewed page or one of its ancestors, get its children
+            if (get_children($child->ID) && (in_array($child->ID, get_post_ancestors($current_page)) || ($child->ID == $current_page->ID))) {
+                $menu .= coenv_base_hierarchical_submenu_get_children($child, $current_page);
+            }
+            $menu .= "</li>\n";
+        }
+        $menu .= "</ul>\n";
+    }
+    return $menu;
+}
+
+function special_tvar_coenv_base_hierarchical_submenu_get_children($post, $current_page) {
+    $menu = '';
+    $exclude = implode(',',coenv_base_menu_exclude());
+    // Get all immediate children of this page
+    $args = array(
+        'sort_order' => 'ASC',
+        'sort_column' => 'menu_order',
+        'hierarchical' => 1,
+        'child_of' => $post->ID,
+        'parent' => $post->ID,
+        'offset' => 0,
+        'post_type' => 'page',
+        'post_status' => 'publish',
+        'exclude' => $exclude
+    ); 
+    $children = get_pages($args);
+    if ($children) {
+        // Include a title here if needed
+        $menu = "\n<ul class=\"side-nav\">\n";
+        foreach ($children as $child) {
+            // If the child is the viewed page or one of its ancestors, highlight it
+            if (in_array($child->ID, get_post_ancestors($current_page)) || ($child->ID == $current_page->ID)) {
+                $menu .= '<li class="active"><a href="' . get_permalink($child) . '" class="active">' . $child->post_title . '</a>';  
             }
             // If the page has children and is the viewed page or one of its ancestors, get its children
             if (get_children($child->ID) && (in_array($child->ID, get_post_ancestors($current_page)) || ($child->ID == $current_page->ID))) {
