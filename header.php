cd9d8bc6503e4ff6cc18064c39991bd4f4ca9904
drewcoll <drewcoll@gmail.com>
2020-05-19 16:21:12 -0700

    top bar bug fixes

--- header.php
+++ header.php
@@ -258,7 +258,6 @@
     <div class="section-row row">
         <div class="columns large-8 section-title"><?php echo coenv_base_section_title($post->ID); ?></div>
     </div>
-    </div>
-</div>
+  </div>
 <?php endif; ?>
 <?php do_action('foundationPress_after_header'); ?>
