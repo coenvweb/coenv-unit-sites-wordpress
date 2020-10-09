b58c5f42c422453f57fabb64c1ca923acd6a80ed
drewcoll <drewcoll@gmail.com>
2020-10-08 17:04:05 -0700

    plugin update

--- wp-content/plugins/revisionary/readme.txt
+++ wp-content/plugins/revisionary/readme.txt
@@ -7,7 +7,7 @@ Tags: revision, submit changes, workflow, collaboration, permissions, moderate, 
 Requires at least: 4.9.7
 Requires PHP: 5.6.20
 Tested up to: 5.5
-Stable tag: 2.3.12
+Stable tag: 2.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -99,6 +99,13 @@ Follow PublishPress on [Facebook](https://www.facebook.com/publishpress), [Twitt
 12. Scheduled Revisions in Publishing Soon list
 
 == Changelog ==
+
+= 2.4 - 1 Oct 2020 =
+* Fixed : Revisors and other limited editors had editor elements hidden when adding a new post, under some site configurations
+* Fixed : Revision publication always set post publish date to current time. Now does only with enabled setting Revisions > Settings > Pending Revisions > Update Publish Date
+* Fixed : Classic Editor - after Preview Changes is clicked, limited editors see "Submit Revision" button recaptioned to "Update"
+* Compat : PublishPress Authors - Authors could not be changed on Edit Revision screen with Gutenberg editor if PublishPress is also active
+* Compat : Gutenberg Ramp - Revision submission UI did not load for post types that have Gutenberg enabled
 
 = 2.3.12 - 27 Aug 2020 =
 * Fixed : WP 5.5 - Post previews did not display correctly for Revisors
--- wp-content/plugins/revisionary/rvy_init.php
+++ wp-content/plugins/revisionary/rvy_init.php
@@ -399,7 +399,7 @@ if (!empty($_REQUEST['rvy_flush_flags'])) {
 function revisionary_refresh_revision_flags() {
 	global $wpdb;
 
-	$status_csv = "'" . implode("','", get_post_stati(['public' => true, 'private' => true], 'names', 'or')) . "'";
+	$status_csv = "'" . implode("','", rvy_filtered_statuses()) . "'";
 	$arr_have_revisions = $wpdb->get_col("SELECT r.comment_count FROM $wpdb->posts r INNER JOIN $wpdb->posts p ON r.comment_count = p.ID WHERE p.post_status IN ($status_csv) AND r.post_status IN ('pending-revision', 'future-revision')");
 	$have_revisions = implode("','", array_map('intval', array_unique($arr_have_revisions)));
 
@@ -974,8 +974,8 @@ function rvy_init() {
 	$revisionary = new Revisionary();
 }
 
-function rvy_is_full_editor($post) {
-	global $current_user;
+function rvy_is_full_editor($post, $args = []) {
+	global $current_user, $revisionary;
 	
 	if (!$type_obj = get_post_type_object($post->post_type)) {
 		return false;
@@ -987,8 +987,17 @@ function rvy_is_full_editor($post) {
 		return false;
 	}
 
-	if (!empty($type_obj->cap->edit_published_posts) && empty($current_user->allcaps[$type_obj->cap->edit_published_posts])) {
-		return false;
+	if (!empty($args['check_publish_caps'])) {
+		if (!empty($type_obj->cap->edit_published_posts) && empty($current_user->allcaps[$type_obj->cap->edit_published_posts])) {
+			return false;
+		}
+	} else {
+		if (empty($revisionary)) {
+			return false;
+		}
+
+		// @todo: skip_revision_allowance?
+		return $revisionary->canEditPost($post, ['simple_cap_check' => true]);
 	}
 
 	return true;
@@ -1026,6 +1035,9 @@ function rvy_preview_url($revision, $args = []) {
 
 	$link_type = rvy_get_option('preview_link_type');
 
+	$status_obj = get_post_status_object(get_post_field('post_status', rvy_post_id($revision->ID)));
+	$post_is_published = $status_obj && (!empty($status_obj->public) || !empty($status_obj->private));
+
 	if ('id_only' == $link_type) {
 		// support using ids only if theme or plugins do not tolerate published post url and do not require standard format with revision slug
 		$preview_url = add_query_arg('preview', true, get_post_permalink($revision));
@@ -1036,7 +1048,7 @@ function rvy_preview_url($revision, $args = []) {
 		} else {
 			$id_arg = 'p';
 		}
-	} elseif ('revision_slug' == $link_type) {
+	} elseif (('revision_slug' == $link_type) || !$post_is_published) {
 		// support using actual revision slug in case theme or plugins do not tolerate published post url
 		$preview_url = add_query_arg('preview', true, get_permalink($revision));
 
@@ -1070,4 +1082,12 @@ function rvy_set_ma_post_authors($post_id, $authors)
 
 	$authors = wp_list_pluck($authors, 'term_id');
 	wp_set_object_terms($post_id, $authors, 'author');
+}
+
+function rvy_filtered_statuses($output = 'names') {
+	return apply_filters(
+		'revisionary_main_post_statuses', 
+		get_post_stati( ['public' => true, 'private' => true], $output, 'or' ),
+		$output
+	);
 }
