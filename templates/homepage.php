147f745c8fc1e3907b1c1e82c2f4793a70360435
drewcoll <drewcoll@gmail.com>
2020-12-09 12:05:48 -0800

    fix homepage category linsk

--- templates/homepage.php
+++ templates/homepage.php
@@ -217,7 +217,7 @@ Template Name: Homepage
 					
 					foreach ($more_terms as &$term) {
 						if ( $term->slug != 'uncategorized') {
-							$more_terms_arr[] = '<a href="/news-and-events/category/ . $term->slug . '">' . $term->name . '</a>';
+							$more_terms_arr[] = '<a href="/news-and-events/category/' . $term->slug . '">' . $term->name . '</a>';
 						}
 					}
 				}
